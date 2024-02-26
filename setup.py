69da2c33d263c7a51df05493ed74a0fd30473ebd
xuxiaohan <413180435@qq.com>
2024-02-26 22:43:05 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Beaconet",
-    version = "1.0.7",
+    version = "1.0.8",
     keywords = ["scRNA-seq","single cell","batch effect","multiple sources","reference-free"],
     description = "A reference-free method for integration of multiple batches of scRNA-seq data.",
     long_description = "the misc-tools for experiment of machine learning and bioinformatics.",
     url = "https://github.com/xuxiaohan",
-    author = "Xu Han, Gao Lin",
+    author = "Xu Han, Yusen Ye, Ran Duan, Yong Gao, Yuxuan Hu*, Gao Lin*",
     author_email = "hxu10670@gmail.com",
     maintainer = "Xu Han",
     maintainer_email = "hxu10670@gmail.com",
